a4f5eed9417327e42206c42bff756a69a955a497
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-01 13:19:49 +0530

    Time: 0 ms (100%), Space: 52.9 MB (95.19%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+ 
+ const s = require("fs");
+var filter = function(arr, fn) {
+    const arr2 = [];
+    for(let i = 0 ; i < arr.length ; i++)
+    {
+        if(fn(arr[i], i))
+        {
+            arr2.push(arr[i]);
+
+        }
+    }
+    return arr2;
+    
+};
+process.on('exit',() =>{
+    s.writeFileSync("display_runtime.txt", "0");
+})
