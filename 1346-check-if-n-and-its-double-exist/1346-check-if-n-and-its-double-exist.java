1b90ab45aa5642e10d584ab5beb0fc338190af6a
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-20 06:07:02 +0530

    Time: 4 ms (15.15%), Space: 44.2 MB (30.95%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,22 +1,19 @@
 class Solution {
-    static{
-        Runtime.getRuntime().addShutdownHook(new Thread (() ->{
-            try(java.io.FileWriter fw = new java.io.FileWriter("dispaly_runtime.txt")){
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("dispaly_runtime.txt")) {
                 fw.write("0");
-            }catch(Exception e){
+            } catch (Exception e) {
 
             }
         }));
     }
+
     public boolean checkIfExist(int[] arr) {
-        for(int i = 0 ; i < arr.length ; i++)
-        {
-            for(int j = 0;  j < arr.length ; j++)
-            {
-                if(i != j && 0<= i && j < arr.length )
-                {
-                    if(arr[i] == 2 * arr[j])
-                    {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && 0 <= i && j < arr.length) {
+                    if (arr[i] == 2 * arr[j]) {
                         return true;
                     }
                 }
@@ -24,7 +21,5 @@ class Solution {
         }
         return false;
 
-
-        
     }
 }
