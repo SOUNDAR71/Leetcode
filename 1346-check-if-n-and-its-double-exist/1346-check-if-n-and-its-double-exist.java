bb47c995478648955463f1191b45fe0081b84847
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-20 06:05:38 +0530

    Time: 4 ms (15.15%), Space: 44.7 MB (14.73%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,30 @@
+class Solution {
+    static{
+        Runtime.getRuntime().addShutdownHook(new Thread (() ->{
+            try(java.io.FileWriter fw = new java.io.FileWriter("dispaly_runtime.txt")){
+                fw.write("0");
+            }catch(Exception e){
+
+            }
+        }));
+    }
+    public boolean checkIfExist(int[] arr) {
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            for(int j = 0;  j < arr.length ; j++)
+            {
+                if(i != j && 0<= i && j < arr.length )
+                {
+                    if(arr[i] == 2 * arr[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+
+        
+    }
+}
