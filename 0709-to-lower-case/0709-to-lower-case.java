6369cd54f4d53377f12c8d866c4a4bc942904eff
SOUNDARKUMAR S <soundarkumar916@gmail.com>
2025-11-28 06:09:29 +0530

    11-28-2025 0709-to-lower-case .java Easy

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String str = "";
+        for(char sr : s.toCharArray())
+        {
+            str = s.toLowerCase()+"";
+        }
+        return str;
+        
+    }
+}
